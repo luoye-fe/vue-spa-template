9d078b8ad14d5da76c65b96e0f9b96e6f475c2f2
luoye <842891024@qq.com>
2016-06-15 11:04:42 +0800

    init

---
+++ template/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
