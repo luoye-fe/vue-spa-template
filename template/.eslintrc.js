fdb66dc7073d34a88adbabdb6739cbeedbbd2877
luoye <842891024@qq.com>
2016-06-28 14:56:00 +0800

    eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -16,8 +16,15 @@ module.exports = {
         'html'
     ],
     'rules': {
-    	indent: ["error", "tab"],
-    	semi: ["error", "always"],
+		"indent": ["error", "tab"],
+		"semi": ["error", "always"],
+	    "space-before-function-paren": ["error", "never"],
+	    "no-extend-native": ["error", {
+	        "exceptions": ["Object", "Date", "String", "Array", "Function"]
+	    }],
+	    "no-unneeded-ternary": ["error", {
+	        "defaultAssignment": false
+	    }],
     {{#if_eq lintConfig "standard"}}
         'arrow-parens': 0,
     {{/if_eq}}
